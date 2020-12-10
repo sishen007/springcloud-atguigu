fe5f79a60f0810f2f76db94afe1cb69a39f048d0
wangyh2 <wangyh2@wedoctor.com>
2020-12-10 15:23:33 +0800

    第十一章代码-服务网关gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * TODO
+ *
+ * @author wangyh2
+ * @version v1.0
+ * @since 2020-12-10 13:46
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+
+}
