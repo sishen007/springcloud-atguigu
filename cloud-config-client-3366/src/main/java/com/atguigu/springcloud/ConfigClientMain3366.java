2b8a291531b90db73b31f2fe1263b5f9dd25169b
wangyh2 <wangyh2@wedoctor.com>
2020-12-14 13:48:21 +0800

    第十四章代码-SpringCloud Bus 消息总线

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * TODO
+ *
+ * @author wangyh2
+ * @version v1.0
+ * @since 2020-12-14 11:12
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,32 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @author wangyh2
+ * @version v1.0
+ * @since 2020-12-14 11:13
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort:"+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+
+}
