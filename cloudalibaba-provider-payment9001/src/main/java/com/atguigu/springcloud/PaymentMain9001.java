e22b6f76ec559d9a6144d082cb0f3ca10907f9b6
wangyh2 <wangyh2@wedoctor.com>
2020-12-16 16:58:53 +0800

    第十七章代码-SpringCloud Alibaba Nacos服务注册和配置中心

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * TODO
+ *
+ * @author wangyh2
+ * @version v1.0
+ * @since 2020-12-15 17:08
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/springcloud/PaymentMain9002.java
@@ -0,0 +1,20 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * TODO
+ *
+ * @author wangyh2
+ * @version v1.0
+ * @since 2020-12-15 17:08
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
